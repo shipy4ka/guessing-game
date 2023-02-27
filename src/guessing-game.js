0c4be7d2db0d8abbe33267c96feb82c62cd87e26
shipy4ka <marinauglanica@gmail.com>
2023-02-27 11:24:33 +0300

    docs: add solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,25 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        const sumOfNum = this.min + this.max;
+        this.midNum = Math.ceil(sumOfNum / 2);
+		return this.midNum;
     }
 
     lower() {
-
+        this.max = this.guess();
     }
 
     greater() {
-
+        this.min = this.guess();
     }
 }
 
